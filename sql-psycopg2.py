8081d0b9a9b6fde5d652eea9fc241b1b8fd376f5
Desislava Naydenova <deskata.01@gmail.com>
2024-06-26 11:27:27 +0000

    Create a new py file for learning about the ORM

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -14,14 +14,24 @@ cursor = connection.cursor()
 #cursor.execute('SELECT "Name" FROM "Artist"')
 
 #Query3 - select only Queen from the artist table
-cursor.execute('SELECT * FROM "Artist"WHERE "Name" = %s',["Queen"])
+#cursor.execute('SELECT * FROM "Artist"WHERE "Name" = %s',["Queen"])
 
+#Query4 - select only by ArtistId #51 from the artist table
+#cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = 51')
+
+#cursor.execute('SELECT * FROM "Artist"WHERE "ArtistId" = %s',[51])
+
+#Query5 - select only the albums with  ArtistId #51 from the album table
+#cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = 51')
+
+#Query - select all tracks where the comppser is Queen from the track table
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
 
 # fetch the results (multiple)
 results = cursor.fetchall()
 
 # fetch the results (single)
-# results = cursor.fetchone()
+#results = cursor.fetchone()
 
 #close the connection
 connection.close()
