aafb835165e0e9a3ac1f380021909cedcf95e9d6
Desislava Naydenova <deskata.01@gmail.com>
2024-06-26 08:45:17 +0000

    Practise with queries from the table

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -7,6 +7,16 @@ connection = psycopg2.connect(database="chinook")
 # build a cursor object to the database
 cursor = connection.cursor()
 
+#Query1 - select all records from the artist table
+#cursor.execute('SELECT * FROM "Artist"')
+
+#Query2 - select only the name column from the artist table
+#cursor.execute('SELECT "Name" FROM "Artist"')
+
+#Query3 - select only Queen from the artist table
+cursor.execute('SELECT * FROM "Artist"WHERE "Name" = %s',["Queen"])
+
+
 # fetch the results (multiple)
 results = cursor.fetchall()
 
