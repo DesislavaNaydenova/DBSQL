496603d852c1d9791855d75125edd19ea41e0e5f
Desislava Naydenova <deskata.01@gmail.com>
2024-06-26 12:28:42 +0000

    Do the 6 queries with psycopg2

--- sql-expression.py
+++ sql-expression.py
@@ -3,11 +3,11 @@ from sqlalchemy import (
 )
 
 #executing the instructions from our localhost "chinok" db
-db = create_engine("postgresql: ///chinook")
+db = create_engine("postgresql:///chinook")
 
 meta = MetaData(db)
 
-#create varible for "Artis" table
+#create varible for "Artist" table
 artist_table = Table(
     "Artist", meta, 
     Column("ArtistId", Integer, primary_key=True),
@@ -28,35 +28,36 @@ track_table = Table(
     Column("AlbumId" ,Integer, ForeignKey("album_table.AlbumId")),
     Column("MediaTypeId", Integer, primary_key=False),
     Column("GenreId", Integer, primary_key=False),
-    Column("Composer", String, ),
+    Column("Composer", String),
     Column("Milliseconds", Integer),
     Column("Bytes", Integer),
-    Column("UnitPrice", Float),
+    Column("UnitPrice", Float)
 )
 
 #making the connection
 with db.connect() as connection:
     #Query1 - select all records from the artist table
-    select_query = artist_table.select()
+    #select_query = artist_table.select()
 
+    #Query2 - select only the name column from the artist table
+    #select_query = artist_table.select().with_only_columns([artist_table.c.Name])
+
+
+    #Query3 - select only Queen from the artist table
+    #select_query = artist_table.select().where(artist_table.c.Name == "Queen")
+   
+
+    #Query4 - select only by ArtistId #51 from the artist table
+    #select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
+     
+    
+    #Query5 - select only the albums with  ArtistId #51 from the album table
+    #select_query = album_table.select().where(album_table.c.ArtistId == 51)
+
+
+    #Query - select all tracks where the comppser is Queen from the track table
+    select_query = track_table.select().where(track_table.c.Composer == "Queen")
+     
     results = connection.execute(select_query)
     for result in results:
         print(result)
-
-
-    #Query2 - select only the name column from the artist table
-#cursor.execute('SELECT "Name" FROM "Artist"')
-
-#Query3 - select only Queen from the artist table
-#cursor.execute('SELECT * FROM "Artist"WHERE "Name" = %s',["Queen"])
-
-#Query4 - select only by ArtistId #51 from the artist table
-#cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = 51')
-
-#cursor.execute('SELECT * FROM "Artist"WHERE "ArtistId" = %s',[51])
-
-#Query5 - select only the albums with  ArtistId #51 from the album table
-#cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = 51')
-
-#Query - select all tracks where the comppser is Queen from the track table
-#cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
