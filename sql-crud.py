15aa932419434961bda1e3f66b8c0aa1fc65c9cb
Desislava Naydenova <deskata.01@gmail.com>
2024-06-28 09:38:16 +0000

    Add the first programmer to our new table with crud

---
+++ sql-crud.py
@@ -0,0 +1,45 @@
+from sqlalchemy import (
+    create_engine, Column, Integer, String
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+#executing the instructions from the "chinook" database
+db = create_engine("postgresql:///chinook")
+base = declarative_base()
+
+#create a class-based model for the "Progremmer" table
+class Programmer(base):
+    __tablename__= "Programmer"
+    id = Column(Integer, primary_key = True)
+    first_name = Column(String)
+    last_name = Column(String)
+    gender = Column(String)
+    nationality = Column(String)
+    famous_for = Column(String)
+
+#instead of cinnecting to tha database directly, we will ask for a session
+#create a new instance of sessionmaker, then point to our engine (the db)
+Session = sessionmaker(db)
+#opens an acrual session by calling the Session() subclass defined above
+session = Session()
+
+#creating the database using declarative_base subclass
+base.metadata.create_all(db)
+
+#creating records on our table
+
+ada_lovelace = Programmer(
+    first_name = "Ada",
+    last_name = "Lovelace",
+    gender = "F",
+    nationality = "British",
+    famous_for = "First Programmer"
+)
+
+#add each instance of our programmers to our session
+session.add(ada_lovelace)
+
+#commit our session to tha database
+session.commit()
