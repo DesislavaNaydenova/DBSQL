0188fa442edb41de8e7b8e80a33a0043364a91d8
Desislava Naydenova <deskata.01@gmail.com>
2024-06-29 09:48:47 +0000

    Delete record

--- sql-crud.py
+++ sql-crud.py
@@ -86,16 +86,33 @@ desislava_naydenova = Programmer(
 # session.commit()
 
 #update multiply records
-people = session.query(Programmer)
-for person in people:
-    if person.gender =="F":
-        person.gender ="female"
-    elif person.gender =="M":
-        person.gender = "male"
-    else:
-        print("Gender not defined")
-    session.commit()
+# people = session.query(Programmer)
+# for person in people:
+#     if person.gender =="F":
+#         person.gender ="female"
+#     elif person.gender =="M":
+#         person.gender = "male"
+#     else:
+#         print("Gender not defined")
+#     session.commit()
 
+#deleting a single record
+fname = input("Enter a first name: ")
+lname = input("Enter a last name: ")
+programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
+#defensive programming
+if programmer is not None:
+    print("Programmer found: ", programmer.first_name + " " + programmer.last_name)
+    confirmation = input("Are you sure you want to delete this record? (y/n)")
+    if confirmation.lower() == "y":
+        session.delete(programmer)
+        session.commit()
+        print("Programmer has been deleted")
+    else:
+        print("Programmer not deleted")
+
+else:
+    print("No record found")
 #query the database to find all programmers
 programmers = session.query(Programmer)
 for programmer in programmers:
